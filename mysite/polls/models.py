41549417cb04f49ab0065b915fdbc195778d3883
kiwib0n <kiwibon@yandex.ru>
2019-04-10 17:24:00 +0300

    Register Question & Group

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
+    def __str__(self):
+        return self.question_text
     question_text = models.CharField(max_length=200)
     pub_date = models.DateField('date published')
 
@@ -9,3 +11,8 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+class Group(models.Model):
+    name = models.CharField(max_length=150)
+    count_subscribes = models.IntegerField(default=0)
+    descriptions = models.CharField(max_length=500)
