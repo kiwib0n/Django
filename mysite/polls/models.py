c423f95898f9fe8adbfef29cf2019f48a350cf02
kiwib0n <kiwibon@yandex.ru>
2019-04-10 18:40:40 +0300

    Output question in display

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,7 +5,7 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateField('date published')
+    pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
