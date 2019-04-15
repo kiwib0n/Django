0bc8556eb3bdecec1b2964ee364b2937c9c6b8fa
dmitriev <dmitriev@keysystems.ru>
2019-04-15 14:49:52 +0300

    Удален неиспользуемый файл

--- mysite/mysite/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
-
-def index(request):
-
-    response = ''
-
-    return HttpResponse(response)
